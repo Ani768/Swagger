5ca3bc600d5dd2bc377e3e2b691ab2b8adbe9d70
Ani768 <143847307+Ani768@users.noreply.github.com>
2024-11-11 15:57:25 +0530

    Update logMiddleware.js

--- middleware/logMiddleware.js
+++ middleware/logMiddleware.js
@@ -1,21 +1,20 @@
 const db = require('../models/index');
-const log = require('../models/log');
 const { Log } = db;
 
 const logAction = async (req, res, next) => {
-    const { id } = req.user; 
-    const action = req.method; 
+    const { id } = req.user || {}; 
+    const action = req.method;
     const resourceType = req.originalUrl.split('/')[1]; 
 
     try {
-       const log= new Log({
-            userId: id, 
+       const log = new Log({
+            userId: id || 'system', 
             action,
-            resourceType
+            resourceType,
+            collection: resourceType, 
+            operationType: action,
         });
         await log.save();
-
-        // return res.status(201).json({ message: 'Log saved' });
     } catch (err) {
         console.error('Failed to log action:', err);
     }
