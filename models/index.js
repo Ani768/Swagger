26a8113b35fd22e9ed021b1a2ad17a6d1acd7a08
Ani768 <143847307+Ani768@users.noreply.github.com>
2024-11-11 15:56:04 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const db = {}
 db.mongoose = mongoose
-db.User = require('./user').User;
+db.User = require('./user');
 db.Log = require('./log');
 module.exports = db;
